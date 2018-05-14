48ef5b99cf710ee95474448a93c80d8a5b166a28
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-05-14 20:30:57 -0300

    add script to move left paddle

---
+++ Assets/Scripts/MovePaddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePaddle : MonoBehaviour {
+
+	public float movingSpeed = 30;
+
+	void FixedUpdate() {
+		float vertPress = Input.GetAxisRaw("Vertical");
+		GetComponent<Rigidbody2D>().velocity = new Vector2(0, vertPress) * movingSpeed;
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
