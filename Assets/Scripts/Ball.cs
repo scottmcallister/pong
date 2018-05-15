f1d29a0b1b09a96c1eed39c78a818e06d69a79d4
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-05-14 21:17:03 -0300

    play sounds and handle collisions

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+	public float ballSpeed = 30;
+
+	private Rigidbody2D rigidBody;
+
+	private AudioSource AudioSource;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D>();
+		rigidBody.velocity = Vector2.right * ballSpeed;
+	}
+	
+	void OnCollisionEnter2D(Collision2D col) {
+		// Left Paddle or Right Paddle
+		if ((col.gameObject.name == "LeftPaddle") || (col.gameObject.name == "RightPaddle")) {
+			HandlePaddleHit(col);
+		}
+
+		// Wall Bottom or Wall Top
+		if ((col.gameObject.name == "WallBottom") || (col.gameObject.name == "WallTop")) {
+			SoundManager.Instance.PlayOneShot(SoundManager.Instance.wallBloop);
+		}
+
+
+		// Left Goal or Right Goal
+		if ((col.gameObject.name == "GoalLeft") || (col.gameObject.name == "GoalRight")) {
+			SoundManager.Instance.PlayOneShot(SoundManager.Instance.goalBloop);
+			
+			// TODO update score
+
+			transform.position = new Vector2(0, 0);
+		}
+	}
+
+	void HandlePaddleHit(Collision2D col) {
+		float y = BallHitPaddleWhere(transform.position, col.transform.position, col.collider.bounds.size.y);
+		Vector2 dir = new Vector2();
+		if(col.gameObject.name == "LeftPaddle") {
+			dir = new Vector2(1, y).normalized;
+		}
+
+		if(col.gameObject.name == "RightPaddle") {
+			dir = new Vector2(-1, y).normalized;
+		}
+
+		rigidBody.velocity = dir * ballSpeed;
+		SoundManager.Instance.PlayOneShot(SoundManager.Instance.hitPaddleBloop);
+	}
+
+	float BallHitPaddleWhere(Vector2 ball, Vector2 paddle, float paddleHeight) {
+		return (ball.y - paddle.y) / paddleHeight;
+	}
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public static SoundManager Instance = null;
+
+	public AudioClip goalBloop;
+	public AudioClip lossBuzz;
+	public AudioClip hitPaddleBloop;
+	public AudioClip winSound;
+	public AudioClip wallBloop;
+
+	private AudioSource soundEffectAudio;
+
+	// Use this for initialization
+	void Start () {
+		if (Instance == null) {
+			Instance = this;
+		} else if (Instance != this) {
+			Destroy(gameObject);
+		}
+
+		AudioSource theSource = GetComponent<AudioSource> ();
+		soundEffectAudio = theSource;
+	}
+
+	public void PlayOneShot(AudioClip clip) {
+		soundEffectAudio.PlayOneShot(clip);
+	}
+}
